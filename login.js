318a98c9dd0a0e48f0efbb384d8296d44ca81aaa
Cyborg-Mcloud <uabashidze@gmail.com>
2020-02-10 09:04:39 +0400

    er
    ee

---
+++ login.js
@@ -0,0 +1,185 @@
+
+
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      fbstatus(response);
+    });
+  }
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '997737107224650',
+      cookie     : true,                     // Enable cookies to allow the server to access the session.
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : 'v4.0'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      fbstatus(response);        // Returns the login status.
+    });
+  };
+
+  
+  (function(d, s, id) {                      // Load the SDK asynchronously
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+ 
+
+  
+function myreg()
+	{
+	console.log("aq var2");
+
+	FB.login(function(response) 
+		{
+	  	console.log(response);
+		if (response["status"]=="connected")
+			{
+			FB.api('/me', {  fields: 'name, email' }, function(response) {
+			console.log(response);
+		   
+			document.getElementById("fbid").value=response.id;
+			document.getElementById("reg_uname").value=response.name;
+			document.getElementById("reg_email").value=response.email;
+				
+			document.getElementById("reg_pass1").style.display="none";
+			document.getElementById("reg_pass2").style.display="none";
+			document.getElementById("fbregbut").style.display="none";
+
+//			submit_form();
+			});
+		}
+
+	}, {scope: 'public_profile,email'});
+		 
+	}
+
+
+
+function mylogin()
+	{
+	console.log("aq var");
+	FB.login(function(response) 
+		{
+		console.log(response);
+		if (response["status"]=="connected")
+			{
+			if (logged==0)
+				{
+				FB.api('/me', {  fields: 'name, email' }, function(response) {
+				console.log(response);
+		   		window.location.href='?fblogin=1&fbid='+response.id + "&email="+response.email;
+				});
+				}
+			}
+		}, {scope: 'public_profile,email'});
+		  	
+	}
+
+
+
+
+function mylogout()
+	{
+
+	if (fblogged==1)
+		{
+		
+		
+		FB.logout(function(response) {
+		   // Person is now logged out
+//		   fb_logged_out();
+			fblogged=0;
+			 fb_logged_out()
+			});
+		}
+
+	if (fblogged==0 && gmlogged==0)
+		{
+		fb_logged_out();
+		}
+	}
+		  
+
+
+
+function fb_logged_out()
+	{
+	window.location.href='?logout=1';
+	}
+
+
+function fbstatus(response)
+	{
+	console.log("aq var 1");
+
+	console.log(response);
+	if (response["status"]=="connected")
+		{
+		fblogged=1;
+
+		if (logged==0 && force_logout==0)
+			{
+			FB.api('/me', {  fields: 'name, email' }, function(response) {
+			  console.log(response);
+			
+			window.location.href='?fblogin=1&fbid='+response.id + "&email="+response.email;
+		
+			});
+			}
+		else if (force_logout==1)		
+			{
+			fbid=1;
+			mylogout();
+			}
+		}
+	}
+
+
+var hideKeyboard = function() 
+{  document.activeElement.blur(); 
+var inputs = document.querySelectorAll('input');  for(var i=0; i < inputs.length; i++) {   inputs[i].blur();  } };
+
+function signin()
+	{
+	document.getElementById("loginForm").submit();
+	}
+
+function checkregform()
+	{
+	if (document.getElementById("reg_email").value=='' || (( document.getElementById("reg_pass1").value=='' || document.getElementById("reg_tel").value=='') && document.getElementById("fbid").value=="")  )
+		{	
+		alert("All Fields are Required!");
+		return false;
+		
+
+		}
+	else
+		{
+		document.getElementById("registerForm").submit();
+		return true;
+
+		}
+
+	}
+
+function show_login()
+	{
+	document.getElementById("logindiv").style.display="block";
+	document.getElementById("regdiv").style.display="none";
+	document.getElementById("login_logo").style.height="200px";
+	}
+function show_reg()
+	{
+	document.getElementById("logindiv").style.display="none";
+	document.getElementById("regdiv").style.display="block";
+	document.getElementById("login_logo").style.height="180px";
+	}
+
---
+++ main.js
@@ -0,0 +1,163 @@
+var players=new Array();
+var max_players=0;
+
+var p_marker= new Array();
+var curmenu=0;
+var targets=new Array(); 
+
+var max_targets=0;
+
+
+var last_coord_time=0;
+var lbut_vis=0;
+var mouse_coords=1;
+var Mouse_Lat=41.7120788;
+var Mouse_Lng=44.7491408;
+
+var selobj=0;
+
+
+var myid="";
+var speed_limit=70;
+var unid="";
+var server_id=0;
+
+
+function draw_players()
+	{
+	if (players.length>max_players)
+		{
+		// new player came
+		for (i=max_players;i<players.length ;i++ )
+			{
+			p_marker[i] = new google.maps.Marker({
+				position: just_latlng,
+				map: MyMap,
+				icon: micon,
+				label: {text: "gamarjoba", color: "white"},
+				animation: google.maps.Animation.DROP,
+				});
+			}
+		max_players=players.length;
+		}
+
+	for (i=0;i<players.length ;i++ )
+		{	
+		var mlabel=p_marker[i].getLabel();
+		mlabel.text=players[i]["uname"];
+		mlabel.color="red";
+
+
+		p_marker[i].setIcon(micon);
+		p_marker[i].setLabel(mlabel);
+		var myLatLng = {lat:  parseFloat(players[i]["lat"]), lng: parseFloat(players[i]["lng"])};
+		p_marker[i].setPosition(myLatLng);
+
+
+		}
+	if (lboard_vis==1)
+		{
+		show_leaders();
+		}
+	
+	}
+
+
+function show_leaders()
+	{
+
+	var takers=targets[0]["takers"];
+
+	var a=new Array();
+	a=takers.split(";");
+
+	document.getElementById("leaderboard_txt").innerHTML="";
+	for (i=0;i<a.length-1 ;i++ )
+		{
+		document.getElementById("leaderboard_txt").innerHTML+=(i+1)+": "+a[i]+"<BR>";
+		}
+	}
+
+function draw_targets()
+	{
+		console.log("drawing targets");
+	var  bounds;
+	var myLatLng = {lat:  MyLat, lng: MyLong};
+	bounds = new google.maps.LatLngBounds();
+	bounds.extend(myLatLng);
+
+	if (targets.length>0)
+		{
+		for (i=0;i<targets.length ;i++ )
+			{	
+			var myLatLng =  {lat:  parseFloat(targets[i]["lat"]), lng: parseFloat(targets[i]["lng"])};
+			t_circle[i].setCenter(myLatLng);
+			minimap_circle[i].setPosition(myLatLng);
+			
+
+			bounds.extend(myLatLng);
+
+			t_circle[i].setRadius(parseFloat(targets[i]["radius"]));
+			
+			if (targets[i]["taken"]==1)
+				{
+				t_circle[i].setOptions({strokeColor: '#00FF00'});
+				}
+			else
+				{
+					t_circle[i].setOptions({strokeColor: '#FF0000'});
+				}
+			
+			}
+
+		document.getElementById("pinfo").innerHTML=targets[0]["name"]+"<BR>distance: "+targets[0]["dist"]+" meters";;
+		var myLatLng =  {lat:  parseFloat(targets[0]["lat"]), lng: parseFloat(targets[0]["lng"])};
+	//	logo_marker.setPosition(myLatLng);
+		Target_marker.setPosition(myLatLng);
+		}
+//	MyMap2.fitBounds(bounds);
+	
+
+	}
+
+function req_players()
+	{
+
+	if (server_id==0)
+		{
+
+			
+		}
+	else
+		{
+		url='https://www.smartgps.ge/letsmove/?update_lat=1';
+		console.log("req players: "+url);
+
+		}
+	
+	brainhttp.open('GET',url,true);
+	brainhttp.send(null);
+	document.getElementById("time_data").innerHTML=Date.now()-last_gps_time;
+
+//console.log("background mode: "+cordova.plugins.backgroundMode.isActive());
+
+	setTimeout("req_players();",1000);
+	}
+
+function show_screen(scrname)
+	{
+	document.getElementById("login").style.display="none";
+	document.getElementById("loading_screen").style.display="none";
+	if (scrname=="login")
+		{
+		document.getElementById("login").style.display="block";
+
+
+
+		}
+
+
+
+
+
+	}
