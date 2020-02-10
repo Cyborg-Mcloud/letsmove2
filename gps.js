b8c8ce0ca24aa0d48d6bd24be76b5750c1deb3ff
Cyborg-Mcloud <uabashidze@gmail.com>
2020-02-10 10:13:09 +0400

    re
    e

---
+++ gps.js
@@ -0,0 +1,31 @@
+var old_tar_lat=0;
+var old_tar_lng=0;
+var last_gps_time=0;
+
+
+var MyMap;
+var MyMarker;
+var MyMarker_Circle;
+var Target_marker;
+
+var micon;
+var old_lat=0;
+var old_lng=0;
+var old_head=0;
+var sent_lat=0;
+var sent_lng=0;
+
+var new_lat=0;
+var new_lng=0;
+
+var spd_lat=0;
+var spd_lng=0;
+var spd_steps=0;
+
+
+var MyLat=41.7120788;
+var MyLong=44.7491408;
+var MyAlt;
+var MyHead;
+var MySpeed;
+var MyAcc;
