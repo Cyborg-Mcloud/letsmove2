97de68b077be284fa142c8ee4edb89df485dd039
Cyborg-Mcloud <uabashidze@gmail.com>
2020-02-10 10:22:52 +0400

    e

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+var curstate="none";
+
+var lboard_vis=0;
+
 var players=new Array();
 var max_players=0;
 
@@ -120,7 +125,95 @@ function draw_targets()
 
 	}
 
-var curstate="none";
+	
+var brainhttp;
+var b_recv;
+if (window.XMLHttpRequest) {brainhttp=new XMLHttpRequest();}
+else if (window.ActiveXObject) {brainhttp=new ActiveXObject('Microsoft.XMLHTTP');}
+else {alert('Your browser does not support XMLHTTP!');}
+
+brainhttp.onreadystatechange=brain_recv;
+
+function brain_recv()
+	{
+
+	if (brainhttp.readyState==4 && brainhttp.status==200)
+		{
+		b_recv=brainhttp.responseText;
+		if (b_recv!="")
+			{
+		
+			obj=JSON.parse(b_recv);
+		//	console.log(obj);
+			players=obj["users"];
+			var new_tars=new Array();
+			if (curmenu==1)
+				{
+				document.getElementById("qulebi").innerHTML=obj["data"]["mpoints"];
+				document.getElementById("rating").innerHTML=obj["data"]["rating"];
+				}
+			document.getElementById("timer").innerHTML=obj["data"]["timer"];
+			document.getElementById("timer2").innerHTML=obj["data"]["timer"];
+			if (typeof obj["targets"] !== 'undefined')
+				{
+				new_tars=obj["targets"];
+
+				if (new_tars.length<targets.length)
+					{
+
+					for (i=new_tars.length;i<targets.length ;i++ )
+						{
+						t_circle[i].setCenter(just_latlng);
+						minimap_circle[i].setPosition(just_latlng);
+						
+						}
+					max_targets=0;
+					targets.length=0;
+					tartgets=[];
+					}
+				targets=obj["targets"];
+				if (targets[0]["taken"]==1 && lbut_vis==0)
+					{
+					document.getElementById("show_leaderb_but").style.display="block";
+					console.log("showing");
+					lbut_vis=1;
+					}
+				else if (targets[0]["taken"]==0 &&  lbut_vis==1)
+					{
+					document.getElementById("show_leaderb_but").style.display="none";
+					console.log("hiding");
+					lbut_vis=0;
+					}
+
+				if (typeof targets[0]["new_take"] !== 'undefined' && targets[0]["new_take"]==1)
+					{
+					console.log("axalio!");
+					document.getElementById("grats_div").style.display='block';
+					}
+				}
+			else
+				{
+				document.getElementById("timer").innerHTML="not started";
+
+
+				}
+
+			
+
+			if (typeof obj["anim"] !== 'undefined')
+				{
+				if (typeof obj["anim"]["take"] !== 'undefined' && obj["anim"]["take"]==1)
+					{
+					console.log("axalio small!");
+					document.getElementById("small_grats_div").style.display='block';
+					}
+				}
+			draw_players();
+			draw_targets();
+			}
+		}
+	}
+
 
 function req_players()
 	{
