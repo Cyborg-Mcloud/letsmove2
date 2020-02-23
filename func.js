eb2392b228ca2958295b2783300871fedfc4fd7d
Cyborg-Mcloud <uabashidze@gmail.com>
2020-02-23 09:09:46 +0400

    e
    e

--- func.js
+++ func.js
@@ -164,7 +164,7 @@ function copy_referal(){
 }
 
 function open_subscribe(){
-	var win=window.open( "https://smartgps.ge/letsmove/pay.php", "_blank");
+	var win=window.open( "https://smartgps.ge/letsmove/pay.php?myid="+myid, "_self");
 	win.addEventListener( "loadstop", function(){
        var loop = window.setInterval(function(){
            win.executeScript({
